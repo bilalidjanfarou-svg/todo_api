87a1b8a79c991d1e317feeb6833b3700e9d93f06
Bilali <bilalidjanfarou@gmail.com>
2026-06-20 10:44:11 +0100

    Add JWT authentication and User model

--- models.py
+++ models.py
@@ -8,3 +8,17 @@ class Todo(Base):
     title = Column(String, index=True)
     description = Column(String, default="")
     completed = Column(Boolean, default=False)
+
+class Todo(Base):
+    __tablename__ = "todo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String,index=True)
+    description = Column(String, default="")
+    completed = Column(Boolean, default=False)
+
+class User(Base):
+    __tablename__ ="user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hashed_password =Column(String)
